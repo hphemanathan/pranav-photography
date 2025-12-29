8ec49956010ef4e4fe4e1ab610b2c85d3ec06884
Hemanathan H P <hphemanathan@gmail.com>
2025-12-29 12:22:30 +0530

    boilerplate

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,6 +4,9 @@ function HeroSection() {
   return (
     <div>
       <p>Pranav Photography</p>
+      <video autoPlay loop muted className='bg-vid'>
+        <source src={BgVideo} type='video/mp4' />{" "}
+      </video>
     </div>
   );
 }
