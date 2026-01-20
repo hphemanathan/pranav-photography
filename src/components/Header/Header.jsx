f6d8bb008df8c613579513bf42fd786af853b3e8
Hemanathan H P <hphemanathan@gmail.com>
2026-01-20 20:02:55 +0530

    wip

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,16 @@ import style from "./Header.module.css";
 
 function Header() {
   return (
-    <div className='flex'>
+    <div className='flex items-center justify-center'>
       <div className='w-[150px]'>
         <img src='src/assets/Logo.png' alt='Logo' />
       </div>
-      <div className='flex ms-auto'>
+      <div className={style.margin}>
+        <div className="flex">
         <p>Services</p>
         <p>My works</p>
         <p>Who I am</p>
+        </div>
         <div>
           <p>Book now</p>
         </div>
