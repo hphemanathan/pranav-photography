335f8df5d6634550d7a6521d889c136ea5b51a3c
Hemanathan H P <hphemanathan@gmail.com>
2026-02-20 05:55:57 +0530

    wip

--- src/components/NavMobile/NavMobile.jsx
+++ src/components/NavMobile/NavMobile.jsx
@@ -5,33 +5,32 @@ import { useGSAP } from '@gsap/react';
 import { Transition } from 'react-transition-group';
 
 
+
+
 function NavMobile({openMenu, setOpenMenu}) {
-
-  const menuRef = React.useRef()
-  const { contextSafe } = useGSAP;
-
-
-  const onEnter = contextSafe ( ( ) => {
-    gsap.fromTo (menuRef.current, 
-      {x:'-100%'},
-      {x: '0%', duration : 0.3, ease: "power3.Out"}
-
-    )
-  }
-
-  )
+ 
+  
 
 
   return (
     <Transition
-    in={openMenu}
-    timeout={300}
+    mountOnEnter
     unmountOnExit
-  onEnter={onEnter}    
+    in={openMenu}
+    addEndListener={(node, done) => {
+      const ctx = gsap.context ( ( ) => {
+        if (openMenu) {
+          gsap.to (".menu", {x:"-50"})
+        }
+      },
+
+      node)
+    }
+
+    }
+    
     >
-      <div
-        ref={menuRef}
-        className='z-50 bg-lightTaupe-500 h-svh absolute top-0 left-0 w-svw'>
+      <div className='z-50 bg-lightTaupe-500 h-svh absolute top-0 left-0 w-svw basic-transition menu'>
         <X
           className='absolute top-[16px] right-[16px]'
           onClick={() => {
