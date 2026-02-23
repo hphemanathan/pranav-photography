ba88d9d36693958a2d6dddca78953afb1dcc6363
Hemanathan H P <hphemanathan@gmail.com>
2026-02-23 12:49:48 +0530

    wip

--- src/components/NavMobile/NavMobile.jsx
+++ src/components/NavMobile/NavMobile.jsx
@@ -34,21 +34,18 @@ function NavMobile({ show = false, close }) {
       addEndListener={(done) => {
         const ctx = gsap.context(() => {
           if (show) {
-            gsap.set('#modal', {
-                xPercent: 100,
-                duration: 0.65,
-                ease: "power3.out",});
+            gsap.set('#modal', {xPercent: 50,});
+            //  gsap.set("#items a", { xPercent: 500 });
             gsap
               .timeline({ onComplete: done })
-              .to("#overlay", { autoAlpha: 0.65, duration: 0.1 })
-              .to("#modal", { autoAlpha: 1, xPercent: 0, duration: 0.65 }, 0);
-
-
-              gsap.to('#modal', {
-                x: 0,
-                duration: 0.4,
-                ease: "power3.out",
+              .to("#modal", { xPercent: 0, duration: 0.5, ease: "power1.out" })
+              .from("#items a", {xPercent: 200, duration: 0.5, stagger: 0.1, ease:"power3.out"})
+              .to("#overlay", {
+                autoAlpha: 0.65,
+                duration: 0.5,
+                ease: "power1.out",
               });
+             
           } else {
             gsap
               .timeline({ onComplete: done })
@@ -62,7 +59,7 @@ function NavMobile({ show = false, close }) {
         id='modal'
         className='z-50 bg-lightTaupe-500 h-svh absolute top-0 left-0 w-svw basic-transition menu'>
         <X className='absolute top-[16px] right-[16px]' />
-        <div className='flex flex-col justify-center items-center h-svh gap-4'>
+        <div id='items' className='flex flex-col justify-center items-center h-svh gap-4'>
           <a href=''>Services</a>
           <a>Packages</a>
           <a>My works</a>
