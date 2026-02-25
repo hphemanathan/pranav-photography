35d1f9174836a90b5de7721d8d7e0fb5e8f515fd
Hemanathan H P <hphemanathan@gmail.com>
2026-02-25 13:10:57 +0530

    wip

--- src/components/NavMobile/NavMobile.jsx
+++ src/components/NavMobile/NavMobile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Transition } from 'react-transition-group';
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-
+import { Menu, X } from 'lucide-react';
 
 function NavMobile({openMenu, setOpenMenu, menuRef}) {
     gsap.registerPlugin(useGSAP);
@@ -12,7 +12,9 @@ function NavMobile({openMenu, setOpenMenu, menuRef}) {
   const onEnter = contextSafe(() => {
     gsap.from(
       menuRef.current, {
-        xPercent:-200
+        xPercent:100,
+        duration:0.7,
+        ease:"power2.out"
       }
     )
   });
@@ -25,9 +27,23 @@ function NavMobile({openMenu, setOpenMenu, menuRef}) {
     mountOnEnter={true}
     onEnter={onEnter}
     >
-  <div ref={menuRef} className='h-svh bg-OffWhite-500'>
-    <p>test</p>
-  </div>
+   <div
+            ref={menuRef}
+            className="z-50 bg-OffWhite-500 h-svh absolute top-0 left-0 w-svw basic-transition menu"
+          >
+            <X
+              className="absolute top-[16px] right-[16px]"
+              onClick={() => {
+                setOpenMenu(!openMenu);
+              }}
+            />
+            <div className="flex flex-col justify-center items-center h-svh gap-4">
+              <a href="">Services</a>
+              <a>Packages</a>
+              <a>My works</a>
+              <a>Who I am</a>
+            </div>
+          </div>
      </Transition>
   )
 }
